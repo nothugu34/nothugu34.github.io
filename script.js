44f15de80288889459d71274b11af7dc94d51dc4
nothugu34 <153878558+nothugu34@users.noreply.github.com>
2023-12-14 19:58:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mama(){
+    alert("Hola mamá, ¿Cómo estás? Se que no hablo mucho contigo");
+    alert("Pero quiero decirte que eres la persona más especial que tengo");
+    alert("No sé qué haría si tú te fueras :(. Por favor, no nos dejes solos :'(");
+    alert("De parte de toda la familia quiero decirte que la pases bien en este día que es tu cumpleaños");
+    alert("Sin nada más que decir, ¡Feliz cumpleaños y que cumplas muchos más años :D!");
+}
